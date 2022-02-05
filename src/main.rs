d0248d6e56578085ba2cc28337653bdd1fed5074
Joshua Meyer <josh.engineer9@gmail.com>
2022-02-05 09:23:06 -0600

    functioning single ticker

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,55 @@ extern crate serde;
 extern crate serde_json;
 
 use dotenv::dotenv;
+use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 use std::env;
-use std::process;
 
-// #[derive(Deserialize, Debug)]
-// struct Json {
+fn float_from_str<'de, D>(deserializer: D) -> Result<f64, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Ok(s.parse::<f64>().unwrap())
+}
+
+fn int_from_str<'de, D>(deserializer: D) -> Result<i32, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Ok(s.parse::<i32>().unwrap())
+}
+
+#[derive(Deserialize, Debug)]
+struct Ticker {
     #[serde(rename = "01. symbol")]
     symbol: String,
-    #[serde(rename = "02. open")]
-    open: String,
-    #[serde(rename = "03. high")]
-    high: String, 
-//     Global Quote": {
-//         "01. symbol": "AAPL",
-//         "02. open": "174.4800",
-//         "03. high": "176.2399",
-//         "04. low": "172.1200",
-//         "05. price": "172.9000",
-//         "06. volume": "89418074",
-//         "07. latest trading day": "2022-02-03",
-//         "08. previous close": "175.8400",
-//         "09. change": "-2.9400",
-//         "10. change percent": "-1.6720%"
-// }
+    #[serde(rename = "02. open", deserialize_with = "float_from_str")]
+    open: f64,
+    #[serde(rename = "03. high", deserialize_with = "float_from_str")]
+    high: f64,
+    #[serde(rename = "04. low", deserialize_with = "float_from_str")]
+    low: f64,
+    #[serde(rename = "05. price", deserialize_with = "float_from_str")]
+    price: f64,
+    #[serde(rename = "06. volume", deserialize_with = "int_from_str")]
+    volume: i32,
+    #[serde(rename = "07. latest trading day")]
+    latest_day: String,
+    #[serde(rename = "08. previous close", deserialize_with = "float_from_str")]
+    previous_close: f64,
+    #[serde(rename = "09. change", deserialize_with = "float_from_str")]
+    change: f64,
+    #[serde(rename = "10. change percent")]
+    change_percent: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv().expect(".env file not found");
+    /// get api_key from alphavantage.co and store it in `.env`
     let api_key = env::var("ALPHA").expect("$ALPHA is not set");
-    println!("{:?}", api_key);
 
     let url = format!(
         "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey={}",
@@ -45,18 +64,15 @@ async fn main() -> Result<(), Error> {
     let response = client.get(url).send().await?;
 
     println!("Status: {}", &response.status());
-    let json_text = response.text().await?;
+    let json_text = &response.text().await?;
     let json_ser_ok = serde_json::from_str(&json_text);
     if !json_ser_ok.is_ok() {
         panic!("Something wrong with Json deserialize.")
     }
     let json_ser: Value = json_ser_ok.unwrap();
 
-    println!("{:?}", json_ser["Global Quote"]);
-    // let result = &response.json::<Json>().await?;
-    // println!("{:?}", &result);
-    // println!("{:?}", &result.data);
-    // println!("{:?}", &result.data[0]);
+    let t: Ticker = serde_json::from_value(json_ser["Global Quote"].clone()).unwrap();
+    println!("{:?}", &t);
 
     Ok(())
 }
