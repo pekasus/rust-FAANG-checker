7fc416e2d4930a843a6542d4504002328233b1a0
pekasus <josh@pekasys.com>
2022-07-02 11:55:58 -0400

    mob next

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,17 @@ use dotenv::dotenv;
 use reqwest::{Client, Error};
 use serde_json;
 use std::env;
-use stocks::faang::{Ticker, get_ticker};
+use stocks::faang::{get_ticker, Ticker};
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv().expect(".env file not found");
-    /// get api_key from alphavantage.co and store it in `.env`
+    // get api_key from alphavantage.co and store it in `.env`
     let api_key = env::var("ALPHA").expect("$ALPHA is not set");
-
-    let url = format!(
-        "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey={}",
-        api_key
-    );
     let client = Client::new();
+    let stocks: Vec<&str> = 
+
     let t = get_ticker(client, "GOOG", api_key).await?;
     println!("{:?}", &t);
 
