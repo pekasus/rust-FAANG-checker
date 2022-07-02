9f235bd0af494ba9bd91025d070a51f7c340f669
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2022-07-02 09:23:23 -0700

    added rayon crate

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use serde_json;
 use std::env;
 use stocks::faang::{get_ticker, Ticker};
 use tokio;
+use rayon::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -11,8 +12,8 @@ async fn main() -> Result<(), Error> {
     // get api_key from alphavantage.co and store it in `.env`
     let api_key = env::var("ALPHA").expect("$ALPHA is not set");
     let client = Client::new();
-    let stocks: Vec<&str> = 
-
+    let stocks: Vec<&str> = vec!["META", "AMZN","AAPL", "NFLX", "GOOG"];
+    stocks.par_iter().map(async move |&t| get_ticker(client, t, api_key).await?);
     let t = get_ticker(client, "GOOG", api_key).await?;
     println!("{:?}", &t);
 
